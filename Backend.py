a959ed31179126eda6203c4e5a89878cbdb990bd
Bella3009 <bellasara13@gmail.com>
2023-06-27 20:50:33 +0200

    Retrieve the name of the event from the website.

--- Backend.py
+++ Backend.py
@@ -2,14 +2,24 @@ import requests
 import selectorlib
 
 url = "http://programmer100.pythonanywhere.com/tours/"
+Headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; '
+                         'Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/39.0.2171.95 Safari/537.36'}
 
 
 def scraper(url):
     """Scrape the page source from the URL"""
-    response = requests.get(url)
+    response = requests.get(url, headers=Headers)
     source = response.text
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
 if __name__ == "__main__":
-    print(scraper(url))
+    scraped = scraper(url)
+    print(extract(scraped))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Music_Event_Webscraper
+
+Retrieve data from http://programmer100.pythonanywhere.com/tours/.
+When there is a new tour the program will send an email as notification.
